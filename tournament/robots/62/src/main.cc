fd4d5b3547721b69139ae3fd6ce3368ce80f3c96
Dan Kang <dartdanfm@gmail.com>
2013-05-17 00:16:54 -0400

    Now a tournament player!  Fixed depth of 7 means it will only work at reasoanble speed for dimension = 4 and even then too slow for tournament play, but it's good enough to beat Alpha Beta Soupa ST with X at 4 (granted it did go over 30 second limit)

--- tournament/robots/62/src/main.cc
+++ tournament/robots/62/src/main.cc
@@ -13,29 +13,43 @@ void printPV(std::vector<PousseMove>& pv) {
 
 
 int main() {
-  PousseGame game(4);
+  int dimension;
+  std::string move;
+  std::cin >> dimension;
+  PousseGame game(dimension);
   std::vector<PousseMove> pv;
-  int value = searchEval(game, 1, PLAYER_X, pv);
-  std::cout << "Level 1: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 2, PLAYER_X, pv);
-  std::cout << "Level 2: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 3, PLAYER_X, pv);
-  std::cout << "Level 3: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 4, PLAYER_X, pv);
-  std::cout << "Level 4: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 5, PLAYER_X, pv);
-  std::cout << "Level 5: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 6, PLAYER_X, pv);
-  std::cout << "Level 6: " << value << std::endl;
-  printPV(pv);
-  value = searchEval(game, 7, PLAYER_X, pv);
-  std::cout << "Level 7: " << value << std::endl;
-  printPV(pv);
+
+  while (std::cin >> move) {
+    game.makeMove(PousseMove(move));
+  }
+
+  searchEval(game, 7, game.turn(), pv);
+  std::cout << pv[0].toString() << std::endl;
+
+
+  // PousseGame game(4);
+  // std::vector<PousseMove> pv;
+  // int value = searchEval(game, 1, PLAYER_X, pv);
+  // std::cout << "Level 1: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 2, PLAYER_X, pv);
+  // std::cout << "Level 2: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 3, PLAYER_X, pv);
+  // std::cout << "Level 3: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 4, PLAYER_X, pv);
+  // std::cout << "Level 4: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 5, PLAYER_X, pv);
+  // std::cout << "Level 5: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 6, PLAYER_X, pv);
+  // std::cout << "Level 6: " << value << std::endl;
+  // printPV(pv);
+  // value = searchEval(game, 7, PLAYER_X, pv);
+  // std::cout << "Level 7: " << value << std::endl;
+  // printPV(pv);
 
 }
 
